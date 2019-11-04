eb6f6977e516d72664d257f6da4f0044eb53eda3
Arnaud <delafontarnaud@hotmail.fr>
2019-11-04 11:08:33 +0100

    commit final du pre-test
    Signed-off-by: Arnaud <delafontarnaud@hotmail.fr>

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -1,23 +1,37 @@
 package com.priceminister.account.implementation;
 
-import com.priceminister.account.*;
-
+import com.priceminister.account.Account;
+import com.priceminister.account.AccountRule;
+import com.priceminister.account.IllegalBalanceException;
 
 public class CustomerAccount implements Account {
 
-    public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
-    }
+	private Double balance;
 
-    public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public void add(Double addedAmount) {
+		if (this.balance == null) {
+			this.balance = addedAmount;
+		} else {
+			this.balance += addedAmount;
+		}
+	}
 
-    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
-    		throws IllegalBalanceException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Double getBalance() {
+		return this.balance == null ? 0.0 : this.balance;
+	}
+
+	@Override
+	public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) throws IllegalBalanceException {
+		double resultingAccountBalance = this.balance - withdrawnAmount;
+
+		if (rule.withdrawPermitted(resultingAccountBalance)) {
+			this.add(-withdrawnAmount);
+		} else {
+			throw (new IllegalBalanceException(resultingAccountBalance));
+		}
+		return this.balance;
+	}
 
 }
--- src/test/java/com/priceminister/account/CustomerAccountTest.java
+++ src/test/java/com/priceminister/account/CustomerAccountTest.java
@@ -1,61 +1,85 @@
 package com.priceminister.account;
 
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.*;
-
-import com.priceminister.account.implementation.*;
-
+import com.priceminister.account.implementation.CustomerAccount;
+import com.priceminister.account.implementation.CustomerAccountRule;
 
 /**
  * Please create the business code, starting from the unit tests below.
- * Implement the first test, the develop the code that makes it pass.
- * Then focus on the second test, and so on.
- * 
- * We want to see how you "think code", and how you organize and structure a simple application.
- * 
- * When you are done, please zip the whole project (incl. source-code) and send it to recrutement-dev@priceminister.com
- * 
+ * Implement the first test, the develop the code that makes it pass. Then focus
+ * on the second test, and so on.
+ *
+ * We want to see how you "think code", and how you organize and structure a
+ * simple application.
+ *
+ * When you are done, please zip the whole project (incl. source-code) and send
+ * it to recrutement-dev@priceminister.com
+ *
  */
 public class CustomerAccountTest {
-    
-    Account customerAccount;
-    AccountRule rule;
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        customerAccount = new CustomerAccount();
-    }
-    
-    /**
-     * Tests that an empty account always has a balance of 0.0, not a NULL.
-     */
-    @Test
-    public void testAccountWithoutMoneyHasZeroBalance() {
-        fail("not yet implemented");
-    }
-    
-    /**
-     * Adds money to the account and checks that the new balance is as expected.
-     */
-    @Test
-    public void testAddPositiveAmount() {
-        fail("not yet implemented");
-    }
-    
-    /**
-     * Tests that an illegal withdrawal throws the expected exception.
-     * Use the logic contained in CustomerAccountRule; feel free to refactor the existing code.
-     */
-    @Test
-    public void testWithdrawAndReportBalanceIllegalBalance() {
-        fail("not yet implemented");
-    }
-    
-    // Also implement missing unit tests for the above functionalities.
+	Account customerAccount;
+	AccountRule rule;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		customerAccount = new CustomerAccount();
+	}
+
+	/**
+	 * Tests that an empty account always has a balance of 0.0, not a NULL.
+	 */
+	@Test
+	public void testAccountWithoutMoneyHasZeroBalance() {
+		Double balance = customerAccount.getBalance();
+		assertTrue(balance != null && balance.doubleValue() == 0.0);
+	}
+
+	/**
+	 * Adds money to the account and checks that the new balance is as expected.
+	 */
+	@Test
+	public void testAddPositiveAmount() {
+		Double balance = customerAccount.getBalance();
+		customerAccount.add(12.5);
+		assertTrue(balance + 12.5 == customerAccount.getBalance());
+	}
+
+	/**
+	 * Tests that an illegal withdrawal throws the expected exception. Use the logic
+	 * contained in CustomerAccountRule; feel free to refactor the existing code.
+	 *
+	 * @throws IllegalBalanceException
+	 */
+	@Test(expected = IllegalBalanceException.class)
+	public void testWithdrawAndReportBalanceIllegalBalance() throws IllegalBalanceException {
+		customerAccount.add(10.0);
+		customerAccount.withdrawAndReportBalance(50.0, new CustomerAccountRule());
+	}
+
+	/**
+	 * Tests that a legal withdrawal does not throw the expected exception and
+	 * checks that the new balance is as expected.
+	 *
+	 */
+	@Test
+	public void testWithdrawAndReportBalanceLegalBalance() throws IllegalBalanceException {
+		customerAccount.add(10.0);
+
+		// test a legal withdrawal
+		customerAccount.withdrawAndReportBalance(5.0, new CustomerAccountRule());
+		assertTrue(customerAccount.getBalance().doubleValue() == 5.0);
+
+		// test the limit of a legal withdrawal when it empties the account
+		customerAccount.withdrawAndReportBalance(5.0, new CustomerAccountRule());
+		assertTrue(customerAccount.getBalance().doubleValue() == 0.0);
+	}
 
 }
